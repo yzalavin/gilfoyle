8893dbbde7990eaadb1941f3c79b3524b7dbbb37
Yan Zalavin <yztrnc@gmail.com>
2016-10-17 17:34:45 +0300

    Change link from github to telegram

--- models/message/error.rb
+++ models/message/error.rb
@@ -4,7 +4,7 @@ module Message
       case Validator.new(current_params).error_type
       when :missing_text then 'We could not encrypt nothing! Provide some text.'
       when :invalid_hours_or_visits
-        'We do not how long to encrypt! Provide hours or/and visits count as an integer value.'
+        'We do not know how long to encrypt! Provide hours or/and visits count as an integer value.'
       when :short_password
         'We do not want to harm your data! Provide password with at least six symbols.'
       end
