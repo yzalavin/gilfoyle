fa08cd2545fe6c3722ec95c37a82a75e91088a0b
Yan Zalavin <yztrnc@gmail.com>
2016-10-17 08:36:32 +0300

    Add template view for showing message

--- controller.rb
+++ controller.rb
@@ -4,11 +4,17 @@ end
 
 post '/message' do
   @message = Message::Creator.new()
-  @link = 'fucking_awesome_link'
+  @link = '/message/fucking_awesome_link'
   erb :final
 end
 
 get '/message/:token' do
+  @message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  if true
+    erb :message
+  else
+    erb :not_found
+  end
   # Message::Finder.new()
 end
 
