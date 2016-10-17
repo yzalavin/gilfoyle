f54164a129cf4a3074134aed369fa8fe2c98a638
Yan Zalavin <yztrnc@gmail.com>
2016-10-17 08:24:49 +0300

    Compose template final view

--- controller.rb
+++ controller.rb
@@ -2,8 +2,16 @@ get '/' do
   erb :main
 end
 
-post '/create_message' do
-  p '-----------------'
-  p params
+post '/message' do
+  @message = Message::Creator.new()
+  @link = 'fucking_awesome_link'
   erb :final
+end
+
+get '/message/:token' do
+  # Message::Finder.new()
+end
+
+def message_params
+  {}.tap { |hash| Message::Base::PARAMS.each { |param| hash[param] = params[param] }}
 end
